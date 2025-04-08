1db8f9f8b982499aed527d8ab0b67f5e2e347b1c
josh <jishjr104@gmail.com>
2025-04-07 19:41:11 -0500

    render get file and read

--- server/firebaseAdmin.js
+++ server/firebaseAdmin.js
@@ -1,9 +1,9 @@
 import admin from "firebase-admin";
-import { readFileSync } from "fs";
 
 // Load service account credentials from environment variable
-const path = process.env.GOOGLE_APPLICATION_CREDENTIALS;
-const serviceAccount = JSON.parse(fs.readFileSync(path, "utf8"));
+// const serviceAccount = require(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+const serviceAccountPath = "/etc/secrets/firebase-admin.json";
+const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, "utf8"));
 
 if (!admin.apps.length) {
   admin.initializeApp({
